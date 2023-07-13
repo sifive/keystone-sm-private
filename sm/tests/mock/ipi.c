2ba903505f96428b4ea86e1e3f754bdf7b94531a
Andy Chiu <andy.chiu@sifive.com>
2023-07-13 01:37:40 -0700

    sort out files to be linked with opensbi
    Remove unnecessary files and directories
    
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>

--- sm/tests/mock/ipi.c
+++
@@ -1,6 +0,0 @@
-#include <sbi/sbi_tlb.h>
-
-void __wrap_send_and_sync_pmp_ipi(int region_idx, int type, uint8_t perm) 
-{
-  return;
-}
--- sm/tests/mock/secure_boot.c
+++
@@ -1,7 +0,0 @@
-#include "crypto.h"
-
-byte __wrap_sanctum_sm_hash[MDSIZE];
-byte __wrap_sanctum_sm_signature[SIGNATURE_SIZE];
-byte __wrap_sanctum_sm_public_key[PRIVATE_KEY_SIZE];
-byte __wrap_sanctum_sm_secret_key[PUBLIC_KEY_SIZE];
-byte __wrap_sanctum_dev_public_key[PUBLIC_KEY_SIZE];
