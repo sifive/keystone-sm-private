878268f005d437d90f7fbccb0aceb47c89e8f32c
Andy Chiu <andy.chiu@sifive.com>
2023-07-13 18:54:14 -0700

    opensbi: make building keystone configurable
    Signed-off-by: Andy Chiu <andy.chiu@sifive.com>

--- objects.mk
+++ objects.mk
@@ -1,34 +1,34 @@
 ifdef PLATFORM
-  platform-genflags-y += "-DTARGET_PLATFORM_HEADER=\"platform/$(PLATFORM)/platform.h\""
+  platform-genflags-$(CONFIG_KEYSTONE) += "-DTARGET_PLATFORM_HEADER=\"platform/$(PLATFORM)/platform.h\""
 else
 	PLATFORM = "generic"
-  platform-genflags-y += "-DTARGET_PLATFORM_HEADER=\"platform/generic/platform.h\""
+  platform-genflags-$(CONFIG_KEYSTONE) += "-DTARGET_PLATFORM_HEADER=\"platform/generic/platform.h\""
 endif
 
-platform-objs-y += keystone/attest.o
-platform-objs-y += keystone/cpu.o
-platform-objs-y += keystone/crypto.o
-platform-objs-y += keystone/enclave.o
-platform-objs-y += keystone/pmp.o
-platform-objs-y += keystone/sm.o
-platform-objs-y += keystone/sm-sbi.o
-platform-objs-y += keystone/sm-sbi-opensbi.o
-platform-objs-y += keystone/thread.o
-platform-objs-y += keystone/mprv.o
-platform-objs-y += keystone/sbi_trap_hack.o
-platform-objs-y += keystone/trap.o
-platform-objs-y += keystone/ipi.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/attest.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/cpu.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/crypto.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/enclave.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/pmp.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/sm.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/sm-sbi.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/sm-sbi-opensbi.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/thread.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/mprv.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/sbi_trap_hack.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/trap.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ipi.o
 
-platform-objs-y += keystone/sha3/sha3.o
-platform-objs-y += keystone/ed25519/fe.o
-platform-objs-y += keystone/ed25519/ge.o
-platform-objs-y += keystone/ed25519/keypair.o
-platform-objs-y += keystone/ed25519/sc.o
-platform-objs-y += keystone/ed25519/sign.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/sha3/sha3.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ed25519/fe.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ed25519/ge.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ed25519/keypair.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ed25519/sc.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/ed25519/sign.o
 
-platform-objs-y += keystone/hkdf_sha3_512/hkdf_sha3_512.o
-platform-objs-y += keystone/hmac_sha3/hmac_sha3.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/hkdf_sha3_512/hkdf_sha3_512.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/hmac_sha3/hmac_sha3.o
 
-platform-objs-y += keystone/platform/$(PLATFORM)/platform.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/platform/$(PLATFORM)/platform.o
 
-platform-objs-y += keystone/plugins/plugins.o
+platform-objs-$(CONFIG_KEYSTONE) += keystone/plugins/plugins.o
