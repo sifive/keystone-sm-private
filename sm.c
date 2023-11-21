sifive/keystone-sm-private

commit 038fad9686d141b9915360a3928128c0d6a3fcd0

keystone: Hide the print message
Hide the Keystone security monitor initilization message when tile power
gating or core complex power gating.

Signed-off-by: Nick Hu <nick.hu@sifive.com>

sm.c
@@ -14,6 +14,7 @@
 #include <sbi/riscv_barrier.h>
 #include <sbi/sbi_console.h>
 #include <sbi/sbi_hart.h>
+#include <sbi_utils/reset/fdt_reset_sifive_susp.h>
 
 static int sm_init_done = 0;
 static int sm_region_id = 0, os_region_id = 0;
@@ -171,16 +172,15 @@ void sm_init(bool cold_boot)
   pmp_set_keystone_dump(sm_region_id, PMP_NO_PERM, cold_boot);
   pmp_set_keystone_dump(os_region_id, PMP_ALL_PERM, cold_boot);
 
-  /* Fire platform specific global init */
-  if (platform_init_global() != SBI_ERR_SM_ENCLAVE_SUCCESS) {
-    sbi_printf("[SM] platform global init fatal error");
-    sbi_hart_hang();
+  if (!(sifive_tile_check_pg_reboot() || sifive_smc_check_pg_reboot())) {
+    /* Fire platform specific global init */
+    if (platform_init_global() != SBI_ERR_SM_ENCLAVE_SUCCESS) {
+      sbi_printf("[SM] platform global init fatal error");
+      sbi_hart_hang();
+    }
+    sbi_printf("[SM] Keystone security monitor has been initialized!\n");
+    sm_print_hash();
   }
-
-  sbi_printf("[SM] Keystone security monitor has been initialized!\n");
-
-  sm_print_hash();
-
   return;
   // for debug
   // sm_print_cert();

commit 76589d383da76c01856389044686390b171258c3

keystone: set wg CSRs on warm boots
This is a potential bug where warm boot will not set wg CSRs to the
non-secure wid. Fix it by calling the platform warm init hook
unconditionally, and only print the log for coldboot inside the hook.

fixes: 038fad9686d ("keystone: Hide the print message")
Signed-off-by: Andy Chiu <andy.chiu@sifive.com>

sm.c
@@ -172,12 +172,13 @@ void sm_init(bool cold_boot)
   pmp_set_keystone_dump(sm_region_id, PMP_NO_PERM, cold_boot);
   pmp_set_keystone_dump(os_region_id, PMP_ALL_PERM, cold_boot);
 
+  if (platform_init_global() != SBI_ERR_SM_ENCLAVE_SUCCESS) {
+    sbi_printf("[SM] platform global init fatal error");
+    sbi_hart_hang();
+  }
+
   if (!(sifive_tile_check_pg_reboot() || sifive_smc_check_pg_reboot())) {
     /* Fire platform specific global init */
-    if (platform_init_global() != SBI_ERR_SM_ENCLAVE_SUCCESS) {
-      sbi_printf("[SM] platform global init fatal error");
-      sbi_hart_hang();
-    }
     sbi_printf("[SM] Keystone security monitor has been initialized!\n");
     sm_print_hash();
   }